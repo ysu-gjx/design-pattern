03812c85ec90b2c35e3ed2c09403b05e5b4c3a12
ysu-gjx <252932110@qq.com>
2020-09-17 09:07:12 +0800

    测试

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-class Person {
-  constructor (name) {
+// 车 父类
+class Car {
+  constructor(number, name) {
+    this.number = number
     this.name = name
   }
-  getName() {
-    return this.name
+}
+// 快车
+class KuaiChe extends Car {
+  constructor(number, name, price) {
+    super(number, name)
+    this.price = price
+  }
+}
+// 专车
+class ZhuanChe extends Car {
+  constructor(number, name, price) {
+    super(number, name)
+    this.price = price
+  }
+}
+// 行程
+class Trip {
+  constructor(car) {
+    this.car = car
+  }
+  start() {
+    console.log(`行程开始，车牌号: ${this.car.number}, 车辆名称： ${this.car.name}`)
+  }
+  end() {
+    console.log(`行程结束，价格为： ${this.car.price * 5}`)
   }
 }
 
-let p = new Person('lee')
+let car = new KuaiChe('京888888', 'BYD', 1)
+let trip = new Trip(car)
 
-alert(p.getName())
+trip.start()
+trip.end()
